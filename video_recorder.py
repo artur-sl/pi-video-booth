0093184960e1c47a1125a87913468fadbe56a63e
Artur Ambroziak <ambroziak.artur@gmail.com>
2021-08-03 22:38:43 +0200

    Add image overlay instead of annotation

--- video_recorder.py
+++ video_recorder.py
@@ -1,6 +1,7 @@
 import threading
 import logging
 from picamera import PiCamera, Color
+from PIL import Image
 
 log = logging.getLogger()
 
@@ -14,17 +15,30 @@ class VideoRecorder:
         self.camera.resolution = (1920, 1080)  # (640, 480)
         self.camera.framerate = 25
         self.camera.rotation = 180
-        self.camera.annotate_background = Color('red')
-        self.camera.annotate_foreground = Color('white')
-        self.camera.annotate_text_size = 70
-        self.camera.annotate_text = ANNOTATION_TEXT
+        #self.camera.annotate_background = Color('red')
+        #self.camera.annotate_foreground = Color('white')
+        #self.camera.annotate_text_size = 70
+        #self.camera.annotate_text = ANNOTATION_TEXT
+        
+        self.overlay_img = Image.open('overlay.png')
         if preview:
             self.camera.start_preview()
+            self.add_overlay()
+
+    def add_overlay(self):
+        self.overlay = self.camera.add_overlay(
+            self.overlay_img.tobytes(), size=self.overlay_img.size, 
+            alpha=0, layer=3
+        )
+        
+    def remove_overlay(self):
+        self.camera.remove_overlay(self.overlay)
         
     def start(self, file_name, file_dir):
         self.lock.acquire()
         file_name = f"{file_dir}/{file_name}.h264"
-        self.camera.annotate_text = ""
+        # self.camera.annotate_text = ""
+        self.remove_overlay()
         log.info('starting video recording: %s', file_name)
         video_thread = threading.Thread(target=self.camera.start_recording, args=(file_name,))
         video_thread.start()
@@ -33,7 +47,8 @@ class VideoRecorder:
         if self.lock.locked():
             self.lock.release()
             self.camera.stop_recording()
-            self.camera.annotate_text = ANNOTATION_TEXT
+            self.add_overlay()
+            # self.camera.annotate_text = ANNOTATION_TEXT
             return True
         else:
             return False
